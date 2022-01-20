55c3b7100f2071775ccd3799c2be96053f750376
madaleineo <90480511+madaleineo@users.noreply.github.com>
2022-01-19 19:49:32 -0700

    initial commit for mission 3

---
+++ Mission2Assignment/Models/GradeCalcModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        [Range(0, 100)]
+
+        public int assignments { get; set; }
+
+        public int groupProjects { get; set; }
+
+        public int quizzes { get; set; }
+
+        public int exams { get; set; }
+
+        public int intex { get; set; }
+    }
+}
